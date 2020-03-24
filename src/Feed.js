5fdd318a12315a9c98d97b8453c1a9288c51026c
LKuenzler <laurin.kue@gmail.com>
2020-03-24 14:27:43 +0100

    Added app feed post

---
+++ src/Feed.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class Feed extends React.Component {
+
+    //Posts aus dem in App.js geladenen rendern
+
+    constructor(props) {
+        super()
+
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+export default Feed
---
+++ src/Post.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Post extends React.Component {
+
+    //Bild + Text anzeigen
+
+    constructor(props) {
+        super()
+    }
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+export default Post
